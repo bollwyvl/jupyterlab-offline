f682cb98e085a1f12d81857ef2759cf90a245725
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-03-17 14:32:01 -0400

    hack more

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-conda build -c conda-forge conda.recipes/jupyterlab
-conda build -c conda-forge conda.recipes/jupyterlab-offline
-conda build -c conda-forge conda.recipes/jupyterlab-offline-jupyter-widgets-jupyterlab-manager
-conda build -c conda-forge conda.recipes/jupyterlab-offline-bqplot
